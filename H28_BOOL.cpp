20b78276075e8a5836d2c5bcbeb2753a1acba7b6
tinSn0703 <condensa1952@gmail.com>
2016-08-28 13:38:18 +0900

    0.4.2
    Revertの戻し方がわかりませんでした(怒)

--- H28_BOOL.cpp
+++ H28_BOOL.cpp
@@ -4,6 +4,12 @@
 #include "H28_BOOL.h"
 
 inline BOOL
+operator ~ (BOOL _arg_bool)
+{
+	return (BOOL)((~(int)_arg_bool) & 1);
+}
+
+inline BOOL
 operator &
 (
 	BOOL _arg_bool_0,
@@ -24,52 +30,52 @@ operator |
 }
 
 template <class T>
-inline BOOL 
-ADD_TF 
+inline BOOL
+ADD_TF
 (
-	T _arg_data_0, 
-	T _arg_data_1 
+	T _arg_data_0,
+	T _arg_data_1
 )
 {
 	return (BOOL)((_arg_data_0 & _arg_data_1) & 1);
 }
 
 template <class T>
-inline BOOL 
+inline BOOL
 TURN_TF (T _arg_turn_data )
 {
-	return (BOOL)((~_arg_turn_data) & 1);
+	return (BOOL)((~(int)_arg_turn_data) & 1);
 }
 
 template <class T>
-inline BOOL 
-CHECK_BIT_TF 
+inline BOOL
+CHECK_BIT_TF
 (
-	uint _arg_check_data, 
-	T _arg_check_bit 
+	int _arg_check_data,
+	T _arg_check_bit
 )
 {
 	return (BOOL)((_arg_check_data >> _arg_check_bit) & 1);
 }
 
 template <class T>
-inline BOOL 
-CHECK_BIT_TF 
+inline BOOL
+CHECK_BIT_TF
 (
-	uint _arg_check_data, 
-	T _arg_check_bit_0, 
-	T _arg_check_bit_1 
+	int _arg_check_data,
+	T _arg_check_bit_0,
+	T _arg_check_bit_1
 )
 {
 	return (BOOL)((_arg_check_data >> _arg_check_bit_0) & (_arg_check_data >> _arg_check_bit_1) & 1);
 }
 
 template <class T>
-inline BOOL 
-CHECK_TURN_BIT_TF 
+inline BOOL
+CHECK_TURN_BIT_TF
 (
-	uint _arg_check_data, 
-	T _arg_check_bit 
+	int _arg_check_data,
+	T _arg_check_bit
 )
 {
 	return TURN_TF(_arg_check_data >> _arg_check_bit);
